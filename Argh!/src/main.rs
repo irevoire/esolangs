658b8363d77b83e8ddc9678d262a185b8cc97c05
Thomas Campistron <irevoire@hotmail.fr>
2019-10-14 04:18:11 +0900

    allow to provide a file as input instead of piping everything in stdin

--- Argh!/src/main.rs
+++ Argh!/src/main.rs
@@ -1,12 +1,28 @@
 mod grid;
 mod vm;
 
-use std::io;
+use std::fs::File;
+use std::io::{stdin, BufReader};
 
 fn main() {
-    let reader = io::stdin();
-    let reader = reader.lock();
-    let grid = grid::Grid::from(reader);
+    let grid;
+    let mut args = std::env::args();
+    let arg = args.nth(1);
+    if let Some(filename) = arg {
+        let file = File::open(filename);
+        if let Err(e) = file {
+            println!("The file specified is invalid: {}", e);
+            return;
+        }
+        let reader = BufReader::new(file.unwrap()); // we can unwrap safely
+        grid = grid::Grid::from(reader);
+    } else {
+        println!("Expect the source code on stdin");
+        let reader = stdin();
+        let reader = reader.lock();
+        grid = grid::Grid::from(reader);
+    }
+
     let mut vm = vm::Vm::new(grid);
 
     loop {
